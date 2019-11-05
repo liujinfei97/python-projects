95f61641ebeee93459d6cc78f1b5d0fb0a4dcf37
liujinfei97 <53684758+liujinfei97@users.noreply.github.com>
2019-11-05 13:10:02 +0700

    Add files via upload

---
+++ HTTPServer/httpsever/setting.py
@@ -0,0 +1,12 @@
+#HTTPSever配置文件
+
+
+#httpserver地址
+HOST ='0.0.0.0'
+PORT = 8000
+ADDR = (HOST,PORT)   #用户可以在这里进行地址以及端口号的配置
+
+#webframe地址
+frame_ip = '127.0.0.1'
+frame_port = 8080
+frame_addr = (frame_ip,frame_port)
